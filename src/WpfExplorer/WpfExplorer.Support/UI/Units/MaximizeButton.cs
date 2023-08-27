e8a92bf97b6ee00644fd3a84d9d45e9bd33df1ce
KevinSung <ssm0725@gmail.com>
2023-08-28 02:31:34 +0900

    studying WpfExplorer

---
+++ src/WpfExplorer/WpfExplorer.Support/UI/Units/MaximizeButton.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfExplorer.Support.UI.Units
+{
+	public class MaximizeButton : Button
+	{
+		static MaximizeButton()
+		{
+			DefaultStyleKeyProperty.OverrideMetadata(typeof(MaximizeButton),
+				new FrameworkPropertyMetadata(typeof(MaximizeButton)));
+		}
+	}
+}

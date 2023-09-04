b704cb1e51bbae6a895bb51e2f11d3d21a14ac74
KevinSung <ssm0725@gmail.com>
2023-09-05 03:22:10 +0900

    add foldertreeview

---
+++ src/WpfExplorer/WpfExplorer.Support/UI/Units/FolderTreeView.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfExplorer.Support.UI.Units
+{
+    public class FolderTreeView : TreeView
+    {
+        static FolderTreeView()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(FolderTreeView),
+                new FrameworkPropertyMetadata(typeof(FolderTreeView)));
+        }
+    }
+}

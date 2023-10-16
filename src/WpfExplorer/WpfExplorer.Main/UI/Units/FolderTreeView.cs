955b34e22a695e045c3cc0587963ea1b072d46d9
KevinSung <ssm0725@gmail.com>
2023-10-17 03:30:28 +0900

    selectioncommand 작업 중

--- src/WpfExplorer/WpfExplorer.Main/UI/Units/FolderTreeView.cs
+++ src/WpfExplorer/WpfExplorer.Main/UI/Units/FolderTreeView.cs
@@ -5,20 +5,42 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
+using WpfExplorer.Support.Local.Models;
 
 namespace WpfExplorer.Main.UI.Units
 {
     public class FolderTreeView : TreeView
     {
+        public static readonly DependencyProperty SelectionCommandProperty =
+            DependencyProperty.Register("SelectionCommand", typeof(ICommand), typeof(FolderTreeView));
+
         static FolderTreeView()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(FolderTreeView),
                 new FrameworkPropertyMetadata(typeof(FolderTreeView)));
         }
 
+        public ICommand SelectionCommand
+        {
+            get => (ICommand)GetValue(SelectionCommandProperty);
+            set => SetValue(SelectionCommandProperty, value);
+        }
+
 		protected override DependencyObject GetContainerForItemOverride()
 		{
             return new FolderTreeItem();
+		}
+
+        public FolderTreeView()
+        {
+			SelectedItemChanged += OnSelectedItemChanged;
+        }
+
+		private void OnSelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+		{
+			if (SelectedItem is FolderInfo item)
+                SelectionCommand?.Execute(item);
 		}
 	}
 }
